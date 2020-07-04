196f5a67b701f27c763d3387486fb9aff2f3f502
Henrik <henrik.jochum@uzh.ch>
2020-07-04 17:48:03 +0200

    currently functioning: score +1 on click, changes after defined time, start button

--- click-squares-game/click-squares.py
+++ click-squares-game/click-squares.py
@@ -1,18 +1,11 @@
 import tkinter
 import random
-import time
-
-s = 0
-time = random.choice(1,5)
+import root
 
 window = tkinter.Tk()
 window.configure(bg = "white")
+window.minsize(500, 500)
 window.title("Click Squares Game")
-
-location1 =
-
-btn1 = tkinter.Button(window, bg = "red", command = s_change)
-btn1.place(height = 50, width = 50, relx = 1.0, rely = 0, anchor="ne")
 
 btn2 = tkinter.Button(window, bg = "black")
 btn2.place(height = 50, width = 50, relx = 0, rely = 1.0, anchor="sw")
@@ -23,21 +16,50 @@ btn3.place(height = 50, width = 50, relx = 0, rely = 0, anchor="nw")
 btn4 = tkinter.Button(window, bg = "black")
 btn4.place(height = 50, width = 50, rely= 1.0, relx = 1.0, anchor="se")
 
-score = tkinter.Label(window, text = s, font=("Helvetica", 40), bg = "white").place(rely= 0.5, relx = 0.5, anchor = "center")
+btn5 = tkinter.Button(window, bg = "black")
+btn5.place(height = 50, width = 50, relx = 1.0, rely = 0, anchor="ne")
+
+btn1 = tkinter.Button(window, bg = "red", command = s_change)
+btn1.place(height = 50, width = 50, relx = 1.0, rely = 0, anchor="ne")
+
+s = 0
+score = tkinter.Label(window, text = s, font = ("Helvetica", 40), bg = "white").place(rely= 0.5, relx = 0.5, anchor = "center")
+
+start = tkinter.Button(window, bg = "red", text = "START", font = ("Helvetica", 20), command = time_change).place(rely= 0.7, relx = 0.5, anchor = "center")
 
 def s_change():
 	global s
 	s = s + 1
 	score = tkinter.Label(window, text = s, font=("Helvetica", 40), bg = "white").place(rely= 0.5, relx = 0.5, anchor = "center")
-	# c_change()
+	click_change()
 	return
 
-def c_change():
-	time.sleep(time)
+def c_change1():
+	btn1.place(height = 50, width = 50, relx = 0, rely = 1.0, anchor="sw")
 
-	if bg == "red":
-		bg = "black"
-	else:
-		bg = "red"
+def c_change2():
+	btn1.place(height = 50, width = 50, relx = 0, rely = 0, anchor="nw")
+
+def c_change3():
+	btn1.place(height = 50, width = 50, rely= 1.0, relx = 1.0, anchor="se")
+
+def c_change4():
+	btn1.place(height = 50, width = 50, relx = 1.0, rely = 0, anchor="ne")
+
+change_list = [c_change1, c_change2, c_change3, c_change4]
+
+def click_change():
+	random.choice(change_list)()
+	return
+
+def rec():
+	click_change()
+	time_change()
+	return
+
+def time_change():
+	btn1.after(1000, click_change)
+	btn1.after(1000, time_change)
+	return
 
 window.mainloop()
